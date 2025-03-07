b75a944be739bf4e06b1fe92c6f5c58b5a3139e1
hhjeee <yoonhj0315@gmail.com>
2025-03-07 20:43:04 +0900

    chart css

--- apps/test/src/App.jsx
+++ apps/test/src/App.jsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import  Chart  from "react-apexcharts";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="backGround">
+      <div className="chart">
+        <Chart
+            width="100%"
+            height="100%"
+            options={{
+                chart: {
+                    type: 'line',
+                },
+                yaxis: [
+                    {
+                        seriesName: 'Income',
+                        labels: {
+                            formatter: function (y) {
+                                return y + " S1";
+                            }
+                        }
+                    },
+                    {
+                        seriesName: ['Revenue', 'Revenue Tax'],
+                        opposite: true,
+                        labels: {
+                            formatter: function (y) {
+                                return y + " S2";
+                            }
+                        }
+                    },
+                ],
+            }}
+            series={[
+                {
+                    name: 'Income',
+                    data: [1.4, 2, 2.5, 1.5, 2.5, 2.8, 3.8, 4.6],
+                },
+                {
+                    name: 'Revenue',
+                    data: [20, 29, 37, 36, 44, 45, 50, 58],
+                },
+                {
+                    name: 'Revenue Tax',
+                    data: [1.1, 3, 3.1, 4, 4.1, 4.9, 6.5, 8.5],
+                },
+            ]}
+          />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
